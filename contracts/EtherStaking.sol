4b8d41b7c6fe39f9d4a06e83b419ad4b8f39d7b9
Sinclair <sinclairolajuwon@gmail.com>
2024-09-15 22:10:29 -0500

    donne with contract i think

--- contracts/EtherStaking.sol
+++ contracts/EtherStaking.sol
@@ -6,6 +6,7 @@ contract EtherSatking{
    error  TimeNotExceeded();
    error ZeroAddressDetected();
    error AlreadyClaimed();
+   error TimeExceededGoClaimRewards();
 
     event staked(address indexed user, uint256 indexed amount, uint256 indexed period);
     event Withdrawn(address indexed user, uint256 indexed amount);
@@ -50,22 +51,41 @@ contract EtherSatking{
          if(msg.sender == address(0)){revert ZeroAddressDetected();}
         if(block.timestamp <= stakers[msg.sender].period){revert TimeNotExceeded();}
         stakers[msg.sender].amount = 0;
+        if(stakers[msg.sender].stillStaked == true){
+            (bool success,) = msg.sender.call{value: stakers[msg.sender].amount + rewardCalc()}("");
+            require(success);
+            emit Withdrawn(msg.sender, stakers[msg.sender].amount + rewardCalc());
+        }else{
+            (bool success,) = msg.sender.call{value: stakers[msg.sender].amount}("");
+            require(success);
+            emit Withdrawn(msg.sender, stakers[msg.sender].amount);
+        }
+       
 
-        (bool success,) = msg.sender.call{value: stakers[msg.sender].amount + rewardCalc()}("");
-        require(success);
+        
 
     }
    
     function claimRewards() external{
          _onlyOwner();
-         if( stakers[msg.sender].stillStaked = false){revert AlreadyClaimed();}
+        if( stakers[msg.sender].stillStaked == false){revert AlreadyClaimed();}
         if(msg.sender == address(0)){revert ZeroAddressDetected();}
         if(block.timestamp <= stakers[msg.sender].period){revert TimeNotExceeded();}
         stakers[msg.sender].stillStaked = false;
         stakers[msg.sender].amount = stakers[msg.sender].amount + rewardCalc();
+        emit RewardClaimed(msg.sender, rewardCalc());
     }
 
-    function emergencyWithdraw() external {}
+    function emergencyWithdraw() external {
+        _onlyOwner();
+        if(msg.sender == address(0)){revert ZeroAddressDetected();}
+        if(block.timestamp >= stakers[msg.sender].period){revert TimeExceededGoClaimRewards();}
+        stakers[msg.sender].amount = 0;
+        (bool success,) = msg.sender.call{value: stakers[msg.sender].amount}("");
+        require(success);
+        emit EmergencyWithdrawn(msg.sender,stakers[msg.sender].amount);
+
+    }
     function viewTotalStaked() external view returns(uint256){
         _onlyOwner();
         return totalStaked;
@@ -74,7 +94,10 @@ contract EtherSatking{
         _onlyOwner();
         return stakers[msg.sender].amount;
     }
-    function viewRewards() external{}
+    function viewRewards() external view returns(uint256){
+        _onlyOwner();
+      return  rewardCalc();
+    }
 
     function _onlyOwner() private view{
         msg.sender == owner;
